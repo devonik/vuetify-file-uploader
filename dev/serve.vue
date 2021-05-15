aae4e40e2650fe92d451a0de3274335745591545
Niklas Grieger <niklas.grieger1@gmail.com>
2021-05-15 23:13:51 +0200

    initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,26 @@
+<script>
+import Vue from 'vue';
+import VuetifyFileUploaderSample from "../src/lib-components/file-uploader";
+import FileUploader from "../src/lib-components/file-uploader";
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {FileUploader, VuetifyFileUploaderSample},
+});
+</script>
+
+<template>
+  <div id="app">
+    <v-app>
+      <v-main>
+        <v-container>
+          <v-card>
+            <v-card-title>File uploader</v-card-title>
+            <v-card-text>
+              <FileUploader></FileUploader>
+            </v-card-text>
+          </v-card>
+        </v-container>
+      </v-main>
+    </v-app>
+  </div>
+</template>
---
+++ src/lib-components/file-uploader.vue
@@ -0,0 +1,25 @@
+
+
+<template>
+  <div class="file-uploader">
+    <v-file-input></v-file-input>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'FileUploader',
+  data() {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  },
+};
+</script>
+
+<style scoped>
+</style>
